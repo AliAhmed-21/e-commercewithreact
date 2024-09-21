2e7ec84b92a27adc2d24bc7a7b824f22ea2f20bc
AliAhmed-21 <ali21.ahmed03@gmail.com>
2024-09-21 16:32:54 +0500

    first commit

---
+++ data.tsx
@@ -0,0 +1,11 @@
+// const handleScroll = () => {
+//   if (
+//     window.innerHeight + document.documentElement.scrollTop !==
+//       document.documentElement.offsetHeight ||
+//     loading
+//   ) {
+//     return;
+//   }
+//   setPage((prevPage) => prevPage + 1);
+// };
+// window.addEventListener("scroll", handleScroll); // Cleanup on component unmount return () => window.removeEventListener('scroll', handleScroll);
